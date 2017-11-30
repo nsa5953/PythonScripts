30a69138b3def1896cdd03cdaa2fbbf0148e4c66
Nilesh Attarde <nilesh_attarde@persistent.co.in>
2017-11-30 16:49:44 +0530

    new file:   while.py 	modified:   set.py

---
+++ while.py
@@ -0,0 +1,15 @@
+#Python Script for While loop
+i=1
+while i < 4 :
+    print('\nOuter loop Iteration :',i)
+    i+=1
+
+    j = 1
+    while j < 4 :
+        print('\tInner loop Iteration :',j)
+        j +=1
+
+        k = 1
+        while k < 4 :
+            print('\tMost Inner Loop Interation :', k)
+            k +=1
