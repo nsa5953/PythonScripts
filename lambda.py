7c97940563eeace4ded096e7809fa43bf049fee8
Nilesh Attarde <nsa5953@gmail.com>
2017-12-04 22:54:24 +0530

    modified:   lambda.py

--- lambda.py
+++ lambda.py
@@ -1,13 +1,13 @@
 def function_1(x):return x ** 2
 def function_2(x): return x ** 3
 def function_3(x): return x ** 4
-
-callbacks = [function_1, function_2, function_3]
+def function_4(x): return x ** 5
+callbacks = [function_1, function_2, function_3, function_4]
 print('\nNamed Function :')
 for function in callbacks :
-    print('Result :', function(3))
+    print('Result :', function(2))
 callbacks = \
-[lambda x : x ** 2, lambda x : x ** 3, lambda x : x ** 4]
+[lambda x : x ** 2, lambda x : x ** 3, lambda x : x ** 4, lambda x : x ** 5]
 print('\nAnonymous Functions :')
 for function in callbacks:
-    print('Results :', function(3))
+    print('Results :', function(2))
