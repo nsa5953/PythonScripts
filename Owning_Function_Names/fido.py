fe065b5cff1de3bde68f7b72b473f61b5dcbd8b2
Nilesh Attarde <nsa5953@gmail.com>
2017-12-06 21:56:40 +0530

    Owning Function Names

---
+++ Owning_Function_Names/fido.py
@@ -0,0 +1,5 @@
+from dog import *
+pet = input('Enter a Pet Name : ')
+bark(pet)
+lick(pet)
+nap(pet)
---
+++ Owning_Function_Names/pooch.py
@@ -0,0 +1,8 @@
+from dog import bark, lick, nap
+
+bark()
+lick()
+nap()
+bark('Pooch')
+lick('Pooch')
+nap('Pooch')
