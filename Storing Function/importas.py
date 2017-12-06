13d9ed41f1633134bb5949b7b309c28bc02caff3
Nilesh Attarde <nsa5953@gmail.com>
2017-12-06 17:39:17 +0530

    Storing Function

---
+++ Storing Function/importas.py
@@ -0,0 +1,10 @@
+# To create alias for importing module
+import cat as dog # Here I am creating alias to cat as dog
+# Pass Function without any argument
+dog.purr()
+dog.lick()
+dog.nap()
+# Now pass function with arguments
+dog.purr('Kitty')
+dog.lick('Kitty')
+dog.nap('Kitty')
