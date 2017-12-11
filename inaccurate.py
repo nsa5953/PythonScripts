0393745e6babb337748dc8c8426ac5e9bfdaaa94
Nilesh Attarde <nsa5953@gmail.com>
2017-12-11 17:28:27 +0530

    Calulate Decimals

---
+++ inaccurate.py
@@ -0,0 +1,26 @@
+#Python Script for Calculating Decimals
+from decimal import *
+
+item = 0.70
+rate = 1.05
+tax = item * rate
+total = item + tax
+
+print('Item:\t', '%.2f' % item)
+print('Tax:\t', '%.2f' % tax)
+print('Total:\t', '%.2f' % total)
+
+print('Iteam:\t', '%.20f' % item)
+print('Tax:\t', '%.20f' % tax)
+print('Total:\t', '%.20f' % total)
+
+# Add Decimal() to show accurate results
+items = Decimal(0.70)
+rates = Decimal(1.05)
+taxs = items * rates
+totals = items + taxs
+print('Items:\t', '%.2f' % items)
+print('Taxs:\t', '%.2f' % taxs)
+print('Totals:\t', '%.2f' % totals)
+
+# Note: Use Decimal() object to calculate monetery Values or anywhare that accuracy is essensial
