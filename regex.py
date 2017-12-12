68ca7cb0c2e85ba788536271b92824e195cfe4af
Nilesh Attarde <nsa5953@gmail.com>
2017-12-12 17:46:52 +0530

    Matching Patter with re module

---
+++ regex.py
@@ -0,0 +1,21 @@
+# Python script for patter matching with re module
+
+from re import *
+
+# Initailize Variable with a regular expression object
+pattern = compile ('(^|\s)[-a-z0-9_.]+@([-a-z0-9]+.)+[a-z]{2,6}(\s|$)')
+
+# Begin function by requesting user input and attempt to match pattern
+def get_address():
+    address = input('Enter your Email Address :')
+    is_valid = pattern.match(address)
+
+# Add indented statement to display appropriate to describe
+# whether the attempt is succeeded
+    if is_valid :
+        print('Valid Address', is_valid.group())
+    else :
+        print('Invalid Address !!, Please retry... \n')
+
+# Call the defined function
+get_address()
