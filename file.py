e1050747875578d5d6d1f23da1e2bb374f1ed656
Nilesh Attarde <nsa5953@gmail.com>
2017-12-13 18:00:44 +0530

    Reading and Writing files

---
+++ file.py
@@ -0,0 +1,21 @@
+# Python Script to reading and writing files
+# Initialize variable with concatenated string containing newline characters
+poem = 'I never saw a man who looked\n'
+poem += 'With such wishful eye\n'
+poem += 'Upon that little tent of blue\n'
+poem += 'Which prisoners call the sky\n'
+
+# Statement to create file object for new textfile "poem.txt" to write content into
+file = open('poem.txt', 'w')
+
+# Statement to write string contained in the variable to text file and close that files
+file.write(poem)
+file.close()
+
+# Statement to create a file obkect fpr existing text file "poem.txt" to read from
+file = open('poem.txt', 'r')
+
+# Display content of text file and close file
+for line in file:
+    print(line, end ='')
+file.close()
