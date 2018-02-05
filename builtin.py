39ae5821b1f0865cf44513eaac03923daff17b1e
Nilesh Attarde <nsa5953@gmail.com>
2018-02-05 18:19:17 +0530

    Examining built-in attributes

---
+++ builtin.py
@@ -0,0 +1,16 @@
+# Python script for examining built-in attributes
+from Bird import *
+
+zola = Bird('Beep, beep!')
+print('\nBuilt-In Instance attributes....')
+for attrib in dir(zola):
+    if attrib[0] == '__':
+        print(attrib)
+
+print('\nClass Directory.... ')
+for item in Bird.__dict__:
+    print(item, ':', Bird.__dict__[item])
+
+print('\nInstance Directory.... ')
+for item in zola.__dict__:
+    print(item,':',zola.__dict__[item])
